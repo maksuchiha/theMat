0aebf978b6d0c9474641860629319a19de3687dc
Maksim Sheko <uchihaboy95@gmail.com>
2022-02-27 08:19:58 +0300

    create project

---
+++ script/script.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const setContainer = document.querySelector('.constructor-set__choice')
+const constructorOther = document.querySelector('.constructor-set__add')
+const footerCheckbox = document.querySelector('.constructor-footer__checkbox')
+const outputSum = document.querySelector('.constructor-footer__sum > span')
+const fillerChoice = document.querySelector('.constructor-filler__choice')
+
+
+const appData = {
+    fullPrice: 0,
+    setPrice: 0,
+    otherServices: 0,
+    accessories: 0,
+    getFullPrice: () => {
+        appData.fullPrice = appData.setPrice + appData.otherServices + appData.accessories
+        outputSum.textContent = appData.fullPrice
+    },
+    addFillerPrice: () => {
+        fillerChoice.querySelectorAll('input').forEach(item => {
+            if (item.checked) {
+                if (appData.setPrice === 10000) {
+                    appData.setPrice = 13000
+                } else if (appData.setPrice === 7000) {
+                    appData.setPrice = 8000
+                } else if (appData.setPrice === 3000) {
+                    appData.setPrice = 4000
+                }
+            }
+        })
+    },
+    init: () => {
+        setContainer.addEventListener('click', (e) => {
+            if (e.target.closest('input')) {
+                if (e.target.checked) {
+                    appData.setPrice = +e.target.value
+                }
+            }
+            appData.addFillerPrice()
+            appData.getFullPrice()
+        })
+        constructorOther.addEventListener('click', (e) => {
+            if (e.target.closest('input')) {
+                if (e.target.checked) {
+                    appData.otherServices += +e.target.value
+                } else {
+                    appData.otherServices -= +e.target.value
+                }
+            }
+            appData.getFullPrice()
+        })
+        footerCheckbox.addEventListener('click', (e) => {
+            if (e.target.closest('input')) {
+                if (e.target.checked) {
+                    appData.accessories += +e.target.value
+                } else {
+                    appData.accessories -= +e.target.value
+                }
+            }
+            appData.getFullPrice()
+        })
+        fillerChoice.addEventListener('click', (e) => {
+            if (e.target.closest('input')) {
+                if (e.target.checked) {
+                    appData.addFillerPrice()
+                }
+            }
+            appData.getFullPrice()
+        })
+    },
+}
+
+appData.init()
